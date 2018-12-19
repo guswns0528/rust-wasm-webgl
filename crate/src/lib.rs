f2e7651f73b0ae2ab8b770d8a79abcb7ce7549e4
Joon <guswns0528@gmail.com>
2018-12-19 22:03:44 +0900

    replace traingle to hexagon and rotate hexagon.

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(duration_as_u128)]
 #[macro_use]
 extern crate cfg_if;
 extern crate web_sys;
@@ -61,43 +62,45 @@ fn resize_canvas(canvas: &web_sys::HtmlCanvasElement) {
     }
 }
 
-fn ortho_matrix(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> [f32; 16]
-{
-    let result: [f32; 16] = [
-        2.0 / (right - left), 0.0, 0.0, 0.0,
-        0.0, 2.0 / (top - bottom), 0.0, 0.0,
-        0.0, 0.0, 2.0 / (near - far), 0.0,
-        (left + right) / (left - right),
-        (top + bottom) / (bottom - top),
-        (far + near) / (near - far),
-        1.0
-    ];
-
-    result
-}
-
 fn window() -> web_sys::Window {
     web_sys::window().unwrap()
 }
 
+fn request_animation_frame(f: &Closure<FnMut()>) {
+    window().request_animation_frame(f.as_ref().unchecked_ref()).unwrap();
+}
+
+fn get_canvas() -> web_sys::HtmlCanvasElement {
+    let window = window();
+    let document = window.document().unwrap();
+    let canvas = document.get_element_by_id("webgl_canvas").unwrap();
+    canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap()
+}
+
+fn gl_context(canvas: &web_sys::HtmlCanvasElement) -> GlContext {
+    canvas.get_context("webgl2").unwrap().unwrap().dyn_into::<GlContext>().unwrap()
+}
+
 use js_sys::*;
 use web_sys::console;
+
+extern crate cgmath;
+use cgmath::{Matrix4, ortho};
+use cgmath::prelude::*;
+use cgmath::conv::*;
+
+use std::cell::RefCell;
+use std::rc::Rc;
 
 // Called by our JS entry point to run the example.
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     set_panic_hook();
 
-    let window = window();
-    let document = window.document().unwrap();
-    let canvas = document.get_element_by_id("webgl_canvas").unwrap();
-    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+    let canvas = get_canvas();
 
     resize_canvas(&canvas);
-
-    let gl = canvas.get_context("webgl2").unwrap().unwrap()
-        .dyn_into::<GlContext>().unwrap();
-
+    let gl = gl_context(&canvas);
     let vertex_shader_code = r#"#version 300 es
 in vec4 a_position;
 in vec4 a_color;
@@ -126,14 +129,22 @@ void main() {
     let program = create_program(&gl, &vertex_shader, &fragment_shader);
     gl.use_program(Some(&program));
 
-    let vertex_buffer = gl.create_buffer().unwrap();
-    gl.bind_buffer(GlContext::ARRAY_BUFFER, Some(&vertex_buffer));
+    let vertex_buffer = gl.create_buffer();
+    gl.bind_buffer(GlContext::ARRAY_BUFFER, vertex_buffer.as_ref());
 
     let memory = wasm_bindgen::memory().dyn_into::<WebAssembly::Memory>()?.buffer();
-    let vertices: [f32; 18] = [
-        0.0, 0.5 - 0.125, 1.0, 1.0, 0.0, 1.0,
-        -0.75 / 1.732, -0.25 - 0.125, 0.0, 1.0, 1.0, 1.0,
-        0.75 / 1.732, -0.25 - 0.125, 1.0, 0.0, 1.0, 1.0,
+
+    let sqrt3: f32 = 1.7320508075688772935274463415059;
+    let tri_height: f32 = 0.7;
+
+    let vertices: [f32; 7 * 6] = [
+        -tri_height / sqrt3, tri_height, 1.0, 0.0, 0.0, 1.0,
+        tri_height / sqrt3, tri_height, 1.0, 1.0, 0.0, 1.0,
+        tri_height * 2.0 / sqrt3, 0.0, 0.0, 1.0, 0.0, 1.0,
+        tri_height / sqrt3, -tri_height, 0.0, 1.0, 1.0, 1.0,
+        -tri_height / sqrt3, -tri_height, 0.0, 0.0, 1.0, 1.0,
+        -tri_height * 2.0 / sqrt3, 0.0, 1.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.5, 0.5, 0.5, 1.0,
     ];
     let vert_loc = vertices.as_ptr() as u32 / 4;
     let vertex_data = Float32Array::new(&memory).subarray(vert_loc, vert_loc + vertices.len() as u32);
@@ -151,14 +162,53 @@ void main() {
     gl.enable_vertex_attrib_array(color_loc);
     gl.vertex_attrib_pointer_with_i32(color_loc, 4, GlContext::FLOAT, false, 24, 8);
 
+    let viewport_matrix: Matrix4<f32> = ortho(-4.0 / 3.0, 4.0 / 3.0, -1.0, 1.0, -1.0, 1.0);
     let world_loc = gl.get_uniform_location(&program, "world");
-    gl.uniform_matrix4fv_with_f32_array(world_loc.as_ref(), false, &mut ortho_matrix(-4.0 / 3.0, 4.0 / 3.0, -1.0, 1.0, -1.0, 1.0));
+    gl.uniform_matrix4fv_with_f32_array(world_loc.as_ref(), false, unsafe { std::slice::from_raw_parts_mut(array4x4(viewport_matrix)[0].as_mut_ptr(), 16)} );
+
+    let index_buffer = gl.create_buffer();
+    gl.bind_buffer(GlContext::ELEMENT_ARRAY_BUFFER, index_buffer.as_ref());
+
+    let indices: [u32; 8] = [6, 0, 1, 2, 3, 4, 5, 0];
+    let index_loc = indices.as_ptr() as u32 / 4;
+    let index_data = Uint32Array::new(&memory).subarray(index_loc, index_loc + indices.len() as u32);
+
+    gl.buffer_data_with_array_buffer_view(GlContext::ELEMENT_ARRAY_BUFFER, &index_data, GlContext::STATIC_DRAW);
 
     gl.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
     gl.clear_color(0.0, 0.0, 0.0, 1.0);
     gl.clear(GlContext::COLOR_BUFFER_BIT);
 
-    gl.draw_arrays(GlContext::TRIANGLES, 0, 3);
+    gl.draw_elements_with_i32(GlContext::TRIANGLE_FAN, 8, GlContext::UNSIGNED_INT, 0);
+
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    {
+        let mut current: Matrix4<f32> = Matrix4::new(
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0,
+        );
+        let mut prev = window().performance().unwrap().now();
+        *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+            let now = window().performance().unwrap().now();
+            let duration = (now - prev) / 1000.0;
+            prev = now;
+            let rotation = Matrix4::from_angle_z(cgmath::Deg((-60.0 * duration) as f32));
+            current = current * rotation;
+            let world = viewport_matrix * current;
+            let world_loc = gl.get_uniform_location(&program, "world");
+            gl.uniform_matrix4fv_with_f32_array(world_loc.as_ref(), false, unsafe { std::slice::from_raw_parts_mut(array4x4(world)[0].as_mut_ptr(), 16)});
+
+            gl.clear(GlContext::COLOR_BUFFER_BIT);
+            gl.draw_elements_with_i32(GlContext::TRIANGLE_FAN, 8, GlContext::UNSIGNED_INT, 0);
+            request_animation_frame(f.borrow().as_ref().unwrap());
+        }) as Box<FnMut()>));
+    }
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
 
     Ok(())
 }
